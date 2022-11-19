c02ddc7bcc8fc580fd18d779b1b5b5c9cfe32c93
Varun Kamath <mail.varunkamath@gmail.com>
2022-11-19 11:42:47 +0530

    Fixed Simulate Button positioning and added a surprise Pull to find out :)
    Fixed Simulate Button positioning in Gate Space

--- src/components/SimulateButton.js
+++ src/components/SimulateButton.js
@@ -20,10 +20,13 @@ export default class SimulateButton extends Component {
         const simButt = document.getElementById('SimButton');
         simButt.innerText = simButt.innerText === "Simulate"?"Simulating":"Simulate";
         this.state.simulating = !this.state.simulating;
+        // const afterSimButton = window.getComputedStyle(simButt,'::after');
         if(this.state.simulating == true) {
+            simButt.style.setProperty('--afterOpacity','0');
             this.state.GateEventId = setInterval(this.broadcastGateEvent,200)
         }
         else {
+            simButt.style.setProperty('--afterOpacity','1');
             clearInterval(this.state.GateEventId);
             eventBus.dispatch("resetGates"); // If this line is commented it becomes pause button
         }
